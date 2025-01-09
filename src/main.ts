edb5e92db58e4f0d1e9ce0d7dffaea7aa430aa15
MRN <mern_k@hotmail.com>
2025-01-09 21:40:31 +0700

    Update package dependencies and integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // สร้างการตั้งค่า Swagger
+  const config = new DocumentBuilder()
+    .setTitle('ML IoT API')
+    .setDescription('API documentation for ML IoT project')
+    .setVersion('1.0')
+    .build();
+
+  // สร้าง Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // ตั้งค่า Swagger UI
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
